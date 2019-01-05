56f39cbb8a682e14aa337a7b5c82cc111a5571c4
Nicolas Di Prima <nicolas@primetype.co.uk>
2019-01-05 18:23:30 +0000

    fix the macro issue

--- src/macros.rs
+++ src/macros.rs
@@ -4,11 +4,24 @@
 /// This performs an in memory serialisation and returns the
 /// buffer wrapped in a [`Result`](../enum.Result.html).
 ///
+/// ```
+/// #[macro_use]
+/// extern crate cbor_event;
+///
+/// # fn main() {
+/// let value = 0u64;
+/// let bytes = cbor!(value).unwrap();
+/// # assert!(bytes == vec![0])
+/// # }
+/// ```
 #[macro_export]
 macro_rules! cbor {
     ($x:expr) => {{
-        ::cbor_event::se::Serializer::new_vec()
-            .serialize(&$x)
-            .map(|s| s.finalize())
+        let mut se = ::cbor_event::se::Serializer::new_vec();
+        let err = se.serialize(&$x).map(|_| ());
+        err.map(|_| se.finalize())
     }};
 }
+
+#[test]
+fn test_macro() {}
