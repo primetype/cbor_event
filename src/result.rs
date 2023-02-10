715b79893d5314a1e06c4ae106d6531ec37494e2
SimonIT <simonit.orig@gmail.com>
2023-02-10 12:39:17 +0100

    Update src/result.rs
    Co-authored-by: Jane Losare-Lusby <jlusby42@gmail.com>

--- src/result.rs
+++ src/result.rs
@@ -1,4 +1,4 @@
 use core::error::Error;
 
 /// `Result` type for CBOR serialisation and deserialisation.
-pub type Result<T> = core::result::Result<T, dyn Error>;
+pub type Result<T> = core::result::Result<T, Box<dyn Error + Send + Sync + 'static>>;
