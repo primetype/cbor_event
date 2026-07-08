ff3ae34fa33a89a41ef80333fb6f1516ce4e5282
Sebastien Guillemot <sebastiengllmt@gmail.com>
2026-07-08 16:42:55 +0900

    remove unused W generic

---
+++ tests/inference_check.rs
@@ -0,0 +1,16 @@
+extern crate cbor_event;
+use cbor_event::se::{
+    serialize_fixed_map, serialize_indefinite_array, serialize_indefinite_map, Serializer,
+};
+use std::collections::BTreeMap;
+
+#[test]
+fn helpers_callable_without_turbofish() {
+    let mut map: BTreeMap<u64, u64> = BTreeMap::new();
+    map.insert(1, 2);
+    let vec = vec![1u64, 2];
+    let mut s = Serializer::new_vec();
+    serialize_fixed_map(map.iter(), &mut s).unwrap();
+    serialize_indefinite_map(map.iter(), &mut s).unwrap();
+    serialize_indefinite_array(vec.iter(), &mut s).unwrap();
+}
